c7938d61055271b4a1f363b772b9fc8becf402b6
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2021-06-19 16:12:01 +1000

    Completed Day 8 Part 2

--- src/day_08.rs
+++ src/day_08.rs
@@ -44,7 +44,39 @@ fn solve_part_1(file_strings: &Vec<String>) -> usize {
 
 #[aoc(day8, part2)]
 fn solve_part_2(file_strings: &Vec<String>) -> usize {
-    unimplemented!();
+    let mut total_code_length: usize = 0;
+    let mut total_mem_length: usize = 0;
+    for s in file_strings {
+        // Escape characters as required for this part
+        let mut s_rep = s.clone();
+        s_rep = s_rep.replace(r#"\"#, r#"\\"#);
+        s_rep = s_rep.replace(r#"""#, r#"\""#);
+        // Increase total code length of strings
+        total_code_length += s_rep.len();
+        // Count number of characters when stored in memory
+        let mut count = 0;
+        let mut i = 0;
+        let s_chars = s_rep.chars().collect::<Vec<char>>();
+        loop {
+            if i >= s_chars.len() {
+                break;
+            }
+            if s_chars[i] == '\\' {
+                if s_chars[i + 1] == '\\' || s_chars[i + 1] == '"' {
+                    i += 2;
+                } else if s_chars[i + 1] == 'x' {
+                    i += 4;
+                }
+            } else {
+                i += 1;
+            }
+            count += 1;
+        }
+        // Remove characters counted as start and end quote
+        count -= 2;
+        total_mem_length += count;
+    }
+    return total_code_length - total_mem_length;
 }
 
 #[cfg(test)]
@@ -57,5 +89,12 @@ mod tests {
         let input = generate_input(&read_to_string("./input/2015/day8.txt").unwrap());
         let result = solve_part_1(&input);
         assert_eq!(1371, result);
+    }
+
+    #[test]
+    fn test_d08_p2_proper() {
+        let input = generate_input(&read_to_string("./input/2015/day8.txt").unwrap());
+        let result = solve_part_2(&input);
+        assert_eq!(2117, result);
     }
 }
